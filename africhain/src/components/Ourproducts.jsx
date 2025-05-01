fc44cf43e94195d282a1cd8f0abd05b28d952a07
davidojo1144 <davco1144@gmail.com>
2025-05-01 23:12:07 +0100

    A Blockchain platform used for Authenticating products between Manufacturers & Final consumers in Africa and Global market

---
+++ africhain/src/components/Ourproducts.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Title from './Title'
+
+const Ourproducts = () => {
+  return (
+    <div className='container pt-10'>
+        <div className='text-2xl text-center'>
+            <Title text1={"OUR"} text2={"PRODUCTS"}/>
+        </div>
+      <div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Ourproducts
--- africhain/src/pages/Home.jsx
+++ africhain/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Ourproducts from '../components/Ourproducts'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Ourproducts/>
     </div>
   )
 }
