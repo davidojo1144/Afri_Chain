f2de505172b7935dd8014317f75676de95c2cda2
davidojo1144 <davco1144@gmail.com>
2025-05-01 23:41:33 +0100

    A Blockchain platform used for Authenticating products between Manufacturers & Final consumers in Africa and Global market

--- africhain/src/assets/assets.js
+++ africhain/src/assets/assets.js
@@ -4,10 +4,30 @@ import logo from "./logo.png"
 import herologo from "./herologo.jpg"
 
 
+import product1 from "./alexandra-gorn-7Mtz-FAcP98-unsplash.jpg"
+import product2 from "./antonin-fels-OdqmOsUgNwk-unsplash.jpg"
+import product3 from "./c-d-x-PDX_a_82obo-unsplash.jpg"
+import product4 from "./domino-studio-164_6wVEHfI-unsplash.jpg"
+import product5 from "./pmv-chamara-MEsWk-dZzlI-unsplash.jpg"
+import product6 from "./reuben-mansell-nwOip8AOZz0-unsplash.jpg"
+
+
 
 export const assets = {
     cancel,
     menu,
     logo,
     herologo
+}
+
+
+
+export const products = {
+    product1,
+    product2,
+    product3,
+    product4,
+    product5,
+    product6
+
 }
--- africhain/src/components/Ourproducts.jsx
+++ africhain/src/components/Ourproducts.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Title from './Title'
+import { products } from '../assets/assets'
 
 const Ourproducts = () => {
   return (
-    <div className='container pt-10'>
-        <div className='text-2xl text-center'>
-            <Title text1={"OUR"} text2={"PRODUCTS"}/>
+    <div className='container pt-20 pb-10'>
+        <div className='mb-10'>
+            <div className='text-2xl text-center'>
+                <Title text1={"OUR"} text2={"PRODUCTS"}/>
+            </div>
+            <p className="text-center max-w-2xl mx-auto text-sm md:text-lg leading-relaxed">
+                Our cutting-edge, proven security solutions are engineered to deliver top-tier brand protection. Customizable to your exact needs, they seamlessly adapt to your product and application demands.
+            </p>
         </div>
       <div>
-        <div>
-
+        <div className='flex md:flex-row flex-col space-x-5'>
+            <img className='w-72 rounded' src={products.product1} alt="" />
+            <img className='w-72 rounded' src={products.product2}  alt="" />
+            <img className='w-72 rounded' src={products.product3}  alt="" />
+            <img className='w-72 rounded' src={products.product4}  alt="" />
+            <img className='w-72 rounded' src={products.product5}  alt="" />
+            <img className='w-72 rounded' src={products.product6}  alt="" />
         </div>
       </div>
     </div>
