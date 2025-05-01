1de6bfb809d4e5626dd080dc905eb862b681b2f1
davidojo1144 <davco1144@gmail.com>
2025-05-01 14:38:18 +0100

    A Blockchain platform used for Authenticating products between Manufacturers & Final consumers in Africa and Global market

--- africhain/src/App.jsx
+++ africhain/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import { AnimatePresence, motion } from 'framer-motion'
 import Home from './pages/Home'
 
 const App = () => {
+
+  const location = useLocation()
+
   return (
-    <AnimatePresence>
-      <div>
-      <Routes>
+    <AnimatePresence mode="wait">
+      <div className='overflow-x-hidden'>
+      <Routes location={location} key={location.pathname}>
         <Route path='/' element={<Home/>}/>
       </Routes>
     </div>
