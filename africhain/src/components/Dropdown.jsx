143959b3c4b303303beee4f8455dd70ff3a10139
davidojo1144 <davco1144@gmail.com>
2025-05-01 22:34:17 +0100

    A Blockchain platform used for Authenticating products between Manufacturers & Final consumers in Africa and Global market

--- africhain/src/components/Dropdown.jsx
+++ africhain/src/components/Dropdown.jsx
@@ -13,11 +13,15 @@ const Dropdown = ({open}) => {
             className=' top-[8] left-0 w-full z-10'>
 
                 <div className='bg-secondary text-primary text-md font-semibold uppercase py-10 m-6 shadow-xl rounded-3xl'>
-                    <ul className='flex flex-col items-center justify-center gap-7'>
-                        <li className='hover:text-primary-light cursor-pointer'>Register product</li>
+                    <ul className='flex flex-col pl-10 justify-center gap-7'>
+                        <div>
+                            <li className='hover:text-primary-light cursor-pointer'>Register product</li>
+                            <div className='border pr-10'></div>
+                        </div>
                         <li className='hover:text-primary-light cursor-pointer'>Sell Product</li>
                         <li className='hover:text-primary-light cursor-pointer'>Verify </li>
                         <li className='hover:text-primary-light cursor-pointer'>About us</li>
+                        <li className='hover:text-primary-light cursor-pointer'>Contact us</li>
                     </ul>
                 </div>
             </motion.div>
