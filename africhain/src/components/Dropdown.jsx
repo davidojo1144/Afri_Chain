aaa74fdfb660dba5d052de89dfbaf731ad37b464
davidojo1144 <davco1144@gmail.com>
2025-05-01 15:35:53 +0100

    A Blockchain platform used for Authenticating products between Manufacturers & Final consumers in Africa and Global market

--- africhain/src/components/Dropdown.jsx
+++ africhain/src/components/Dropdown.jsx
@@ -1,11 +1,28 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import React from 'react'
 
 const Dropdown = ({open}) => {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <AnimatePresence>
+    {
+        open && (
+            <motion.div
+            initial={{ opacity:0, y: -100 }}
+            animate={{ opacity:1, y: 0 }}
+            exit={{ opacity:0, y: -100 }}
+            transition={{duration: 0.3}}
+            className=' top-[8] left-0 w-full z-10'>
+                <div className='bg-blue-500 text-white text-sm font-semibold uppercase py-10 m-6 rounded-3xl md:hidden'>
+                    <ul className='flex flex-col items-center justify-center  gap-7'>
+                        <li>Upload product</li>
+                        <li>Sell</li>
+                        <li>About</li>
+                        <li>About</li>
+                    </ul>
+                </div>
+            </motion.div>
+        )
+    }
+  </AnimatePresence>
 }
 
 export default Dropdown
--- africhain/src/components/Navbar.jsx
+++ africhain/src/components/Navbar.jsx
@@ -9,13 +9,15 @@ const Navbar = () => {
 
   return (
     <>
-      <div className='container pt-10'>
-        <div className='flex items-center justify-between'>
-            <img className='w-10' onClick={()=> setOpen(!open)} src={open ? assets.cancel : assets.menu} alt="" />
-            <img className='w-32' src={assets.logo} alt="" />
+    <div className='bg-white shadow-2xl'>
+        <div className='container pt-2 pb-2 '>
+            <div className='flex items-center justify-between'>
+                <img className='w-10' onClick={()=> setOpen(!open)} src={open ? assets.cancel : assets.menu} alt="" />
+                <img className='w-32' src={assets.logo} alt="" />
+            </div>
         </div>
-        <Dropdown open={open}/>
-      </div>
+    </div>
+    <Dropdown open={open}/>
     </>
   )
 }
