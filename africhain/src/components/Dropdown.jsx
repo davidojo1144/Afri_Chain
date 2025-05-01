ce99bc5f98b6161f584943b27bb6d9952979b3c6
davidojo1144 <davco1144@gmail.com>
2025-05-01 23:05:33 +0100

    A Blockchain platform used for Authenticating products between Manufacturers & Final consumers in Africa and Global market

--- africhain/src/components/Dropdown.jsx
+++ africhain/src/components/Dropdown.jsx
@@ -12,10 +12,11 @@ const Dropdown = ({open}) => {
             transition={{duration: 0.3}}
             className=' top-[8] left-0 w-full z-10'>
 
-                <div className='bg-secondary text-primary text-md font-semibold uppercase py-10 m-6 shadow-xl rounded-3xl'>
-                    <ul className='flex flex-col pl-10 justify-center gap-7'>
+                <div className='bg-secondary text-primary md:text-md text-xs font-semibold uppercase py-10 m-6 shadow-xl rounded-3xl'>
+                    <ul className='flex flex-col md:pl-10 pl-5 justify-center gap-7'>
                         <div className='space-y-3'>
-                            <li className='hover:text-primary-light cursor-pointer'>Register product</li>
+                            <li className='hover:text-primary-light cursor-pointer'>Registers Product & Generates QR Code
+                            </li>
                             <div className='border w-[90%] pr-10 bg-primary-dark'></div>
                         </div>
                         <div className='space-y-3'>
@@ -23,7 +24,7 @@ const Dropdown = ({open}) => {
                             <div className='border w-[90%] pr-10 bg-primary-dark'></div>
                         </div>
                         <div className='space-y-3'>
-                            <li className='hover:text-primary-light cursor-pointer'>verify</li>
+                            <li className='hover:text-primary-light cursor-pointer'>verify products</li>
                             <div className='border w-[90%] pr-10 bg-primary-dark'></div>
                         </div>
                         <div className='space-y-3'>
@@ -32,6 +33,10 @@ const Dropdown = ({open}) => {
                         </div>
                         <div className='space-y-3'>
                             <li className='hover:text-primary-light cursor-pointer'>contact us</li>
+                            <div className='border w-[90%] pr-10 bg-primary-dark'></div>
+                        </div>
+                        <div className='space-y-3'>
+                            <li className='hover:text-primary-light cursor-pointer'>Displays QR Code & "Authenticity Verified" Badge</li>
                             <div className='border w-[90%] pr-10 bg-primary-dark'></div>
                         </div>
                     </ul>
--- africhain/src/components/Hero.jsx
+++ africhain/src/components/Hero.jsx
@@ -3,10 +3,10 @@ import { assets } from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div className='container pt-10'>
-      <div className='flex md:flex-row flex-col items-center justify-evenly'>
+    <div className='container md:pt-10 pt-5'>
+      <div className='flex md:flex-row flex-col items-center justify-evenly md:space-y-0 space-y-5'>
         <div className='space-y-3'>
-            <h1 className='text-4xl prata-regular'>We fight your counterfeits and increase your revenue</h1>
+            <h1 className='md:text-4xl text-2xl prata-regular'>We fight your counterfeits and increase your revenue</h1>
             <p className='md:text-xl text-sm'>Combat counterfeiting and unauthorized sales while enhancing direct customer engagement.</p>
         </div>
         <img className='w-80 rounded' src={assets.herologo} alt="" />
