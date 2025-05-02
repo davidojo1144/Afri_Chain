bc83459e79e958c31cf41b851dca7ab6a2df912d
davidojo1144 <davco1144@gmail.com>
2025-05-02 06:19:01 +0100

    A Blockchain platform used for Authenticating products between Manufacturers & Final consumers in Africa and Global market

--- africhain/src/components/Contactus.jsx
+++ africhain/src/components/Contactus.jsx
@@ -4,9 +4,9 @@ const Contactus = () => {
   return (
     <div>
       <div className='container p-20 mt-20 mb-20 rounded-2xl text-secondary-light bg-primary-light'>
-        <form action="">
-            <div>
-                <p>Any questions?</p>
+        <div className='flex md:flex-row flex-col gap-10'>
+            <div className='space-y-5'>
+                <p className='prata-regular md:text-3xl text-xl'>Any Questions?</p>
                 <p>We would love to assist you!</p>
                 <p>Schedule a meeting with our brand protection experts to start <br/>protecting your brand and increase your revenue. </p>
                 <div>
@@ -14,8 +14,17 @@ const Contactus = () => {
                     <p>+49 6221 33507 10</p>
                 </div>
             </div>
-            <div></div>
-        </form>
+            <form action="">
+                <div>
+                    <input type="text" />
+                    <input type="text" />
+                </div>
+                <input type="text" />
+                <input type="text" />
+                <textarea name="" id="" cols="30"></textarea>
+                <button>Submit</button>
+            </form>
+        </div>
       </div>
     </div>
   )
