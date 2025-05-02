8d8273e081d7d5e1e715d1c9c379bfe3a3e37e6d
davidojo1144 <davco1144@gmail.com>
2025-05-02 05:35:27 +0100

    A Blockchain platform used for Authenticating products between Manufacturers & Final consumers in Africa and Global market

--- africhain/src/components/Industries.jsx
+++ africhain/src/components/Industries.jsx
@@ -3,10 +3,10 @@ import { industries } from '../assets/assets'
 
 const Industries = () => {
   return (
-    <div className='container pt-10'>
+    <div className='container pt-20'>
         <h1 className='text-center prata-regular text-2xl'>We protect you in these industries</h1>
       <div>
-        <div className='grid md:grid-cols-3 grid-rows-1 gap-10'>
+        <div className='grid md:grid-cols-3 grid-rows-1 gap-10 mt-5'>
             <div className='bg-secondary-light p-10 h-64 rounded-2xl text-primary-light space-y-5'>
                 <p className='md:text-xl text-md'>Wines & spirit</p>
                 <img className='w-60 rounded-xl' src={industries.wines} alt="" />
