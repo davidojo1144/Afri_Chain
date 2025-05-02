3d337d9f87b33a6c8c631a9ffcf4fbf49636e887
davidojo1144 <davco1144@gmail.com>
2025-05-02 05:47:43 +0100

    A Blockchain platform used for Authenticating products between Manufacturers & Final consumers in Africa and Global market

--- africhain/src/components/Industries.jsx
+++ africhain/src/components/Industries.jsx
@@ -8,28 +8,28 @@ const Industries = () => {
       <div>
         <div className='grid md:grid-cols-3 grid-rows-1 gap-10 mt-5'>
             <div className='bg-secondary-dark shadow-2xl md:p-10 p-5 md:h-64 rounded-2xl text-primary-light space-y-5'>
-                <p className='text-xl '>Wines & spirit</p>
-                <img className='w-60 rounded-xl' src={industries.wines} alt="" />
+                <p className='text-xl'>Wines & spirit</p>
+                <img className='md:w-60 rounded-xl' src={industries.wines} alt="" />
             </div>
-            <div className='bg-secondary-light p-10 h-64 rounded-2xl text-primary-light space-y-5'>
-                <p className='md:text-xl text-md'>Licensing</p>
-                <img className='w-60 rounded-xl' src={industries.licensing} alt="" />
+            <div className='bg-secondary-dark shadow-2xl md:p-10 p-5 md:h-64 rounded-2xl text-primary-light space-y-5'>
+                <p className='text-xl'>Licensing</p>
+                <img className='md:w-60 rounded-xl' src={industries.licensing} alt="" />
             </div>
-            <div className='bg-secondary-light p-10 h-64 rounded-2xl text-primary-light space-y-5'>
-                <p className='md:text-xl text-md'>Cosmetics</p>
-                <img className='w-60 rounded-xl' src={industries.cosmetics} alt="" />
+            <div className='bg-secondary-dark shadow-2xl md:p-10 p-5 md:h-64 rounded-2xl text-primary-light space-y-5'>
+                <p className='text-xl'>Cosmetics</p>
+                <img className='md:w-60 rounded-xl' src={industries.cosmetics} alt="" />
             </div>
-            <div className='bg-secondary-light p-10 h-64 rounded-2xl text-primary-light space-y-5'>
-                <p className='md:text-xl text-md'>Baby nutrition</p>
-                <img className='w-48 rounded-xl' src={industries.babynutrition} alt="" />
+            <div className='bg-secondary-dark shadow-2xl md:p-10 p-5 md:h-64 rounded-2xl text-primary-light space-y-5'>
+                <p className='text-xl'>Baby nutrition</p>
+                <img className='md:w-48 rounded-xl' src={industries.babynutrition} alt="" />
             </div>
-            <div className='bg-secondary-light p-10 h-64 rounded-2xl text-primary-light space-y-5'>
-                <p className='md:text-xl text-md'>Health care</p>
-                <img className='w-60 rounded-xl' src={industries.healthcare} alt="" />
+            <div className='bg-secondary-dark shadow-2xl md:p-10 p-5 md:h-64 rounded-2xl text-primary-light space-y-5'>
+                <p className='text-xl'>Health care</p>
+                <img className='md:w-60 rounded-xl' src={industries.healthcare} alt="" />
             </div>
-            <div className='bg-secondary-light p-10 h-64 rounded-2xl text-primary-light space-y-5'>
-                <p className='md:text-xl text-md'>Agricultural products</p>
-                <img className='w-52 rounded-xl' src={industries.agriculture} alt="" />
+            <div className='bg-secondary-dark shadow-2xl md:p-10 p-5 md:h-64 rounded-2xl text-primary-light space-y-5'>
+                <p className='text-xl'>Agricultural products</p>
+                <img className='md:w-52 rounded-xl' src={industries.agriculture} alt="" />
             </div>
         </div>
       </div>
