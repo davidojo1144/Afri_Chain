cc6bb7e3964f0bcb900c478025a43389651452ee
davidojo1144 <davco1144@gmail.com>
2025-05-02 07:05:59 +0100

    A Blockchain platform used for Authenticating products between Manufacturers & Final consumers in Africa and Global market

--- africhain/src/App.jsx
+++ africhain/src/App.jsx
@@ -1,22 +1,17 @@
 import React from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
 import { AnimatePresence, motion } from 'framer-motion'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 
 const App = () => {
 
-  const location = useLocation()
-
 
   return (
-    <AnimatePresence mode="wait">
-      <div className='overflow-x-hidden'>
+    <AnimatePresence>
+      <motion.div className='overflow-x-hidden'>
         <Navbar/>
-      <Routes location={location} key={location.pathname}>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-    </div>
+        <Home/>
+    </motion.div>
     </AnimatePresence>
   )
 }
--- africhain/src/main.jsx
+++ africhain/src/main.jsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 import AfrichainContextProvider from './context/AfrichainContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
     <AfrichainContextProvider>
       <App />
     </AfrichainContextProvider>
-  </BrowserRouter>,
 )
