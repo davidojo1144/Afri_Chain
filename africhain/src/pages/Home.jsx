204acfabb8866dfb0f2b11cf6d05e8d07e052c5f
Biokes <106444765+Biokes@users.noreply.github.com>
2025-05-02 11:07:33 +0100

    homepage

--- africhain/src/pages/Home.jsx
+++ africhain/src/pages/Home.jsx
@@ -4,8 +4,9 @@ import Ourproducts from '../components/Ourproducts'
 import Companies from '../components/Companies'
 import Benefits from '../components/Benefits'
 import Industries from '../components/Industries'
-import Hero from '../components/Hero'
+import Hero from '../components/hero'
 import Contactus from '../components/Contactus'
+
 const Home = () => {
   return (
     <div>
